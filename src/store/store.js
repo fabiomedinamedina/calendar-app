8f5287e94fd8fb2c44d4de969722180a25e20d6f
Fabio Medina <disenofabiomedina@gmail.com>
2023-05-25 13:41:54 -0500

    Fix hidden devtools Redux, Add OG y TWCard, Change favicon

--- src/store/store.js
+++ src/store/store.js
@@ -11,5 +11,6 @@ export const store = configureStore({
   },
   middleware: ( getDefaultMiddleware ) => getDefaultMiddleware({
     serializableCheck: false
-  })
+  }),
+  devTools: import.meta.env.MODE !== 'production',
 })
